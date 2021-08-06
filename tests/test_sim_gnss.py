a3e41fe22ce8edfd26e7cc94b222410f21a4dce7
kanhereashwin <kanhereashwin@users.noreply.github.com>
2021-08-06 15:18:26 -0700

    Updated imports in tests

--- tests/test_sim_gnss.py
+++ tests/test_sim_gnss.py
@@ -5,11 +5,11 @@ import os, sys
 import datetime
 import pytz
 
-from gnss_lib.coordinates import geodetic2ecef
-from gnss_lib.constants import gpsconsts
-from gnss_lib import sim_gnss
-from gnss_lib.utils import datetime_to_tow
-from gnss_lib.ephemeris_manager import EphemerisManager
+from gnss_lib_py.core.coordinates import geodetic2ecef
+from gnss_lib_py.core.constants import GPSConsts
+from gnss_lib_py.core import measures
+from gnss_lib_py.core.ephemeris import datetime_to_tow
+from gnss_lib_py.core.ephemeris import EphemerisManager
 
 
 # Defining test fixtures
@@ -38,7 +38,7 @@ def extract_ephem():
 def static_test_measures(deltaT=0):
     gpsweek = 2105
     _, gpstime = datetime_to_tow(timestamp())
-    measurement, satXYZV = sim_gnss.simulate_measures(gpsweek, gpstime + deltaT, extract_ephem(), Rx_ECEF(), 0., 0., np.zeros([3, 1]))
+    measurement, satXYZV = measures.simulate_measures(gpsweek, gpstime + deltaT, extract_ephem(), Rx_ECEF(), 0., 0., np.zeros([3, 1]))
     return measurement, satXYZV
 
 
@@ -59,7 +59,7 @@ def test_extract_XYZ():
     svs = np.array(['G01', 'G02', 'G03'])
     comb_array = np.hstack((times, pos_array, vel_array))
     test_XYZV = pd.DataFrame(data= comb_array, index=svs, columns=['times', 'x', 'y', 'z', 'vx', 'vy', 'vz'])
-    prns, test_pos, test_vel = sim_gnss._extract_pos_vel_arr(test_XYZV)
+    prns, test_pos, test_vel = measures._extract_pos_vel_arr(test_XYZV)
     np.testing.assert_array_equal(test_pos, pos_array)
     np.testing.assert_array_equal(test_vel, vel_array)
     np.testing.assert_array_equal(prns, np.array([1, 2, 3]))
@@ -67,16 +67,16 @@ def test_extract_XYZ():
 
 
 def test_find_elaz():
-    GPSConst = gpsconsts()
-    Rx = np.reshape(np.array([GPSConst.a, 0, 0]), [1, 3])
+    gpsconsts = GPSConsts()
+    Rx = np.reshape(np.array([gpsconsts.A, 0, 0]), [1, 3])
     test_XYZ = np.zeros([6, 3])
-    test_XYZ[0,0] = 1.25*GPSConst.a
-    test_XYZ[1,0] = 0.75*GPSConst.a
-    test_XYZ[2,0] = GPSConst.a; test_XYZ[2,1]= GPSConst.a
-    test_XYZ[3,0] = GPSConst.a; test_XYZ[3,1]= -GPSConst.a
-    test_XYZ[4,0] = GPSConst.a; test_XYZ[4,2]= GPSConst.a
-    test_XYZ[5,0] = GPSConst.a; test_XYZ[5,2]= -GPSConst.a
-    calc_elaz = sim_gnss.find_elaz(Rx, test_XYZ)
+    test_XYZ[0,0] = 1.25*gpsconsts.A
+    test_XYZ[1,0] = 0.75*gpsconsts.A
+    test_XYZ[2,0] = gpsconsts.A; test_XYZ[2,1]= gpsconsts.A
+    test_XYZ[3,0] = gpsconsts.A; test_XYZ[3,1]= -gpsconsts.A
+    test_XYZ[4,0] = gpsconsts.A; test_XYZ[4,2]= gpsconsts.A
+    test_XYZ[5,0] = gpsconsts.A; test_XYZ[5,2]= -gpsconsts.A
+    calc_elaz = measures.find_elaz(Rx, test_XYZ)
     expect_elaz = np.zeros([6, 2])
     expect_elaz[:, 0] = np.array([90, -90, 0, 0, 0, 0])
     expect_elaz[:, 1] = np.array([0., 0., 90, -90, 0, 180])
@@ -84,13 +84,13 @@ def test_find_elaz():
 
 
 def test_measures_value_range():
-    GPSConst = gpsconsts()
+    gpsconsts = GPSConsts()
     measurements, satXYZV = static_test_measures()
     assert np.logical_and(measurements['prange'].values > 20000e3, measurements['prange'].values < 3e7).all(), "Invalid range of pseudorange values"
     assert np.logical_and(measurements['doppler'].values > -5000, measurements['doppler'].values < 5000).all(), "Magnitude of doppler values is greater than 5 KHz"
-    assert np.all(np.abs(satXYZV['x']).values < GPSConst.a + 2e7), "Invalid range of ECEF x for satellite position"
-    assert np.all(np.abs(satXYZV['y']).values < GPSConst.a + 2e7), "Invalid range of ECEF y for satellite position"
-    assert np.all(np.abs(satXYZV['z']).values < GPSConst.a + 2e7), "Invalid range of ECEF z for satellite position"
+    assert np.all(np.abs(satXYZV['x']).values < gpsconsts.A + 2e7), "Invalid range of ECEF x for satellite position"
+    assert np.all(np.abs(satXYZV['y']).values < gpsconsts.A + 2e7), "Invalid range of ECEF y for satellite position"
+    assert np.all(np.abs(satXYZV['z']).values < gpsconsts.A + 2e7), "Invalid range of ECEF z for satellite position"
 
 def test_satV():
     deltaT = 0.1
