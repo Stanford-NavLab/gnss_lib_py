2f2ecd0eb865f3fbf86be999cc662423cb494a1f
Bradley <bcollico@stanford.edu>
2021-10-15 15:09:36 -0700

    Updated variable names, docstrings. Needs revision to finalize data input format.

--- gnss_lib_py/algorithms/snapshot.py
+++ gnss_lib_py/algorithms/snapshot.py
@@ -1,12 +1,12 @@
 ########################################################################
-# Author(s):    Shubh Gupta
+# Author(s):    Shubh Gupta, Bradley Collicott
 # Date:         19 July 2021
 # Desc:         Point solution methods using GNSS measurements
 ########################################################################
 
 import os
 import sys
-# append <path>/gnss_lib_py/gnss_lib_py/ to path
+# append <path>/gnss_libpy/gnss_libpy/ to path
 sys.path.append(os.path.dirname(
                 os.path.dirname(
                 os.path.realpath(__file__))))
@@ -15,34 +15,41 @@ import numpy as np
 from core.constants import GPSConsts
 
 
-def solve_pos(prange, X, Y, Z, B, e=1e-3):
+def solvepos(
+    prange_measured:np.ndarray, 
+    x_sv:np.ndarray, 
+    y_sv:np.ndarray, 
+    z_sv:np.ndarray, 
+    bias_clock_sv:np.ndarray, 
+    e:float=1e-3
+):
     # TODO: Modify code to perform WLS if weights are given
+    # TODO: Change inputs to either DataFrame or Matrix
     """Find user position, clock bias using WLS or NR methods
 
     Find user position, clock bias by solving the weighted least squares
-    (WLS) problem for m satellites. If no weights are given, the Newton
+    (WLS) problem for n satellites. If no weights are given, the Newton
     Raphson (NR) position, clock bias solution is used instead.
 
     Parameters
     ----------
-    prange : ndarray
-        Measured pseudoranges, dimension m
-    X : ndaarray
-        Satellite ECEF x positions, dimension m
-    Y : ndaarray
-        Satellite ECEF y positions, dimension m
-    Z : ndaarray
-        Satellite ECEF z positions, dimension m
-    B : ndaarray
-        Satellite onboard clock bias, dimension m
+    prange_measured : ndarray
+        Measured pseudoranges, dimension n
+    x_sv : ndaarray
+        Satellite ECEF x positions, dimension n, units [m]
+    y_sv : ndaarray
+        Satellite ECEF y positions, dimension n, units [m]
+    z_sv : ndaarray
+        Satellite ECEF z positions, dimension n, units [m]
+    bias_clock_sv : ndaarray
+        Range bias due to satellite clock offset (c*dt), dimension n, units [m]
     e : float
-        Termination threshold for LS solver
+        Termination threshold for LS solver, units [~]
 
     Returns
     -------
-    x_fix : ndarray
-        Solved 3D position and clock bias estimates
-
+    X_fix : ndarray
+        Solved 3D position and clock bias estimates, dimension 1-by-4, units [m] and [ms]
     """
 
     def _f(vars):
@@ -52,18 +59,25 @@ def solve_pos(prange, X, Y, Z, B, e=1e-3):
         ----------
         vars : list
             List of estimates for position and time
+        x_fix : float
+            User ECEF x position estimate, scalar, units [m]
+        y_fix : float
+            User ECEF y position estimate, scalar, units [m]
+        z_fix : float
+            User ECEF z position estimate, scalar, units [m]
+        bias_clock_user : float
+            Range bias due to user clock offset (c*dt), scalar, units [m]
 
         Returns
         -------
-        delta_prange: ndarray
+        deltaprange: ndarray
             Float difference between expected and measured pseudoranges
-
         """
-        x, y, z, cdt = list(vars)
-        tilde_prange = np.sqrt((x - X)**2 + (y - Y)**2 + (z - Z)**2)
-        _prange = tilde_prange + cdt - B
-        delta_prange = prange-_prange
-        return delta_prange
+        x_fix, y_fix, z_fix, bias_clock_user = list(vars)
+        range_geometric = np.sqrt((x_fix - x_sv)**2 + (y_fix - y_sv)**2 + (z_fix - z_sv)**2)
+        prange_expected = range_geometric + bias_clock_user - bias_clock_sv
+        deltaprange = prange_measured - prange_expected
+        return deltaprange
 
     def _df(vars):
         """Jacobian of expected pseudorange
@@ -72,34 +86,43 @@ def solve_pos(prange, X, Y, Z, B, e=1e-3):
         ----------
         vars : list
             List of estimates for position and time
+        x_fix : float
+            User ECEF x position estimate, scalar, units [m]
+        y_fix : float
+            User ECEF y position estimate, scalar, units [m]
+        z_fix : float
+            User ECEF z position estimate, scalar, units [m]
+        bias_clock_user : float
+            Range bias due to user clock offset (c*dt), scalar, units [m]
 
         Returns
         -------
         derivatives: ndarray
-            mx4 Jacobian matrix of expected pseudorange
+            Jacobian matrix of expected pseudorange, dimension n-by-4
         """
-        x, y, z, cdt = list(vars)
-        tilde_prange = np.sqrt((x - X)**2 + (y - Y)**2 + (z - Z)**2)
-        _prange = tilde_prange + cdt - B
-        derivatives = np.zeros((len(prange), 4))
-        derivatives[:, 0] = -(x - X)/tilde_prange
-        derivatives[:, 1] = -(y - Y)/tilde_prange
-        derivatives[:, 2] = -(z - Z)/tilde_prange
+        x_fix, y_fix, z_fix, bias_clock_user = list(vars)
+        range_geometric = np.sqrt((x_fix - x_sv)**2 + (y_fix - y_sv)**2 + (z_fix - z_sv)**2)
+        derivatives = np.zeros((len(prange_measured), 4))
+        derivatives[:, 0] = -(x_fix - x_sv)/range_geometric
+        derivatives[:, 1] = -(y_fix - y_sv)/range_geometric
+        derivatives[:, 2] = -(z_fix - z_sv)/range_geometric
         derivatives[:, 3] = -1
         return derivatives
 
     gpsconsts = GPSConsts()
-    if len(prange)<4:
+    if len(prange_measured)<4:
         return np.empty(4)
-    x, y, z, cdt = 100., 100., 100., 0.
+    # Inital guess for position estimate and clock offset bias
+    x_fix, y_fix, z_fix, bias_clock_user = 100., 100., 100., 0.
 
-    x0 = np.array([x, y, z, cdt])
-    x_fix, res_err = newton_raphson(_f, _df, x0, e=e)
-    x_fix[-1] = x_fix[-1]*1e6/gpsconsts.C
+    x0 = np.array([x_fix, y_fix, z_fix, bias_clock_user])
+    X_fix, res_err = newtonraphson(_f, _df, x0, e=e)
+    # Return user clock bias in units [ms]
+    X_fix[-1] = X_fix[-1]*1e6/gpsconsts.C
 
-    return x_fix
+    return X_fix
 
-def newton_raphson(f, df, x0, e=1e-3, lam=1., max_count = 20):
+def newtonraphson(f, df, x0, e=1e-3, lam=1., max_count = 20):
     """Newton Raphson method to find zero of function.
 
     Parameters
@@ -123,7 +146,6 @@ def newton_raphson(f, df, x0, e=1e-3, lam=1., max_count = 20):
         Solution for zero of function.
     f_norm : float
         Norm of function magnitude at solution point.
-
     """
     delta_x = np.ones_like(x0)
     count = 0
