a2c0ced2b6adaa4ed4a2bdd26e00ac270e1070a5
betaBison <knowles.derek@gmail.com>
2021-10-26 12:13:28 -0700

    update build docs, remote __init__.py

--- build_docs.sh
+++ build_docs.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 cd docs
 echo "Rebuilding References"
-rm -v ./source/reference/algorithms/*
-poetry run sphinx-apidoc -o ./source/reference/algorithms/ ./../gnss_lib_py/algorithms/
-rm -v ./source/reference/core/*
-poetry run sphinx-apidoc -o ./source/reference/core/ ./../gnss_lib_py/core/
-rm -v ./source/reference/io/*
-poetry run sphinx-apidoc -o ./source/reference/io/ ./../gnss_lib_py/io/
-rm -v ./source/reference/utils/*
-poetry run sphinx-apidoc -o ./source/reference/utils/ ./../gnss_lib_py/utils/
+rm -rv ./source/reference/algorithms/*
+poetry run sphinx-apidoc -f -M -o ./source/reference/algorithms/ ./../gnss_lib_py/algorithms/
+rm -rv ./source/reference/core/*
+poetry run sphinx-apidoc -f -M -o ./source/reference/core/ ./../gnss_lib_py/core/
+rm -rv ./source/reference/io/*
+poetry run sphinx-apidoc -f -M -o ./source/reference/io/ ./../gnss_lib_py/io/
+rm -rv ./source/reference/utils/*
+poetry run sphinx-apidoc -f -M -o ./source/reference/utils/ ./../gnss_lib_py/utils/
 echo "Cleaning up existing make"
 poetry run make clean
 echo "Building docs in html"
