666bd3096a5efe98f580c1ce4b07d05545974c4f
Bradley Collicott <bcollico@stanford.edu>
2021-11-12 13:09:52 -0800

    updated header for test_snapshot

--- tests/algorithms/test_snapshot.py
+++ tests/algorithms/test_snapshot.py
@@ -1,9 +1,7 @@
-########################################################################
-# Author(s):    Bradley Collicott
-# Date:         22 October 2021
-# Desc:         Test for snapshot positioning algorithm.
-########################################################################
 """Test for snapshot positioning algorithm."""
+__authors__ = "Bradley Collicott"
+__date__ = "22 October 2021"
+
 import pytest
 import numpy as np
 from gnss_lib_py.algorithms import snapshot
