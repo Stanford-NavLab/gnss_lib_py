79c673dc35c2b93f7a156553b09bae31bb3667c1
Bradley Collicott <bcollico@stanford.edu>
2021-11-16 01:08:38 -0800

    updated test fixtures and added test for corrections to test_measures

--- tests/core/test_measures.py
+++ tests/core/test_measures.py
@@ -1,16 +1,23 @@
-import pytest
-import numpy as np
-import pandas as pd
+"""Tests for measures.
+
+"""
+
+__authors__ = "Ashwin Kanhere"
+__date__ = "6 Aug 2021"
+
 import os
 import sys
+import datetime
 # append <path>/gnss_lib_py/gnss_lib_py/ to path
 sys.path.append(os.path.dirname(
                 os.path.dirname(
                 os.path.dirname(
                 os.path.realpath(__file__)))))
-# Path of package for pytest
-import datetime
+
 import pytz
+import pytest
+import numpy as np
+import pandas as pd
 
 from gnss_lib_py.core.coordinates import geodetic2ecef
 from gnss_lib_py.core.constants import GPSConsts
@@ -19,7 +26,8 @@ from gnss_lib_py.core.ephemeris import datetime_to_tow
 from gnss_lib_py.core.ephemeris import EphemerisManager
 
 
-# Defining test fixtures
+gpsconsts = GPSConsts()
+dt = 0.1
 
 def timestamp():
     return datetime.datetime(2020, 5, 15, 0, 47, 15, 448796, pytz.UTC)
@@ -33,11 +41,10 @@ def ephem_man():
 
     return EphemerisManager(ephemeris_data_directory)
 
-
 def Rx_ECEF():
-    x_LLA = np.reshape([37.427112, -122.1764146, 16], [1, 3])
-    x_ECEF = np.reshape(geodetic2ecef(x_LLA), [3, 1])
-    return x_ECEF
+    rx_lla  = np.reshape([37.427112, -122.1764146, 16], [1, 3])
+    rx_ecef = np.reshape(geodetic2ecef(rx_lla), [3, 1])
+    return rx_ecef
 
 def extract_ephem():
     manager = ephem_man()
@@ -45,16 +52,27 @@ def extract_ephem():
     ephemeris = manager.get_ephemeris(timestamp(), sats)
     return ephemeris
 
-def static_test_measures(deltaT=0):
+def simulate_test_measures(deltaT=0):
     gpsweek = 2105
     _, gpstime = datetime_to_tow(timestamp())
 
     #NOTE: Calling measures to generate measurements to test measures?
-    measurement, satXYZV = measures.simulate_measures(
-        gpsweek, gpstime + deltaT,extract_ephem(), Rx_ECEF(), 
+    measurement, sat_posvel = measures.simulate_measures(
+        gpsweek, gpstime + deltaT, extract_ephem(), Rx_ECEF(), 
         0., 0., np.zeros([3, 1]))
 
-    return measurement, satXYZV
+    return measurement, sat_posvel
+
+# Define test fixtures
+@pytest.fixture(name="get_meas")
+def fixture_get_meas():
+    measurement, sat_posvel = simulate_test_measures()
+    return measurement, sat_posvel
+
+@pytest.fixture(name="get_meas_dt")
+def fixture_get_meas_dt():
+    measurement, sat_posvel = simulate_test_measures(deltaT=dt)
+    return measurement, sat_posvel
 
 @pytest.fixture(name="set_XYZ")
 def fixture_set_XYZ():
@@ -70,41 +88,52 @@ def fixture_set_XYZ():
     ])
     times = np.array([[1.], [1.], [1.]])
     return pos_array, vel_array, times
-# Defining tests
 
+@pytest.fixture(name="expected_elaz")
+def fixture_expected_elaz():
+    expect_elaz = np.array([[90.0, -90.0, 0.0 ,  0.0 , 0.0, 0.0  ],
+                            [0.0 ,  0.0 , 90.0, -90.0, 0.0, 180.0]]).T
+    return expect_elaz
+
+@pytest.fixture(name="set_sat_pos")
+def fixture_set_sat_pos():
+    sat_pos = np.zeros([6, 3])
+    sat_pos[0,0] = 1.25*gpsconsts.A
+    sat_pos[1,0] = 0.75*gpsconsts.A
+    sat_pos[2,0] = gpsconsts.A; sat_pos[2,1]= gpsconsts.A
+    sat_pos[3,0] = gpsconsts.A; sat_pos[3,1]= -gpsconsts.A
+    sat_pos[4,0] = gpsconsts.A; sat_pos[4,2]= gpsconsts.A
+    sat_pos[5,0] = gpsconsts.A; sat_pos[5,2]= -gpsconsts.A
+    return sat_pos
+
+@pytest.fixture(name="set_rx_pos")
+def fixture_set_rx_pos():
+    Rx = np.reshape(np.array([gpsconsts.A, 0, 0]), [1, 3])
+    return Rx
+
+# Define tests
 def test_extract_XYZ(set_XYZ):
     pos_array, vel_array, times = set_XYZ
-    svs = np.array(['G01', 'G02', 'G03'])
+    sv_names = np.array(['G01', 'G02', 'G03'])
     comb_array = np.hstack((times, pos_array, vel_array))
-    test_XYZV = pd.DataFrame(data= comb_array, index=svs,
-                             columns=['times', 'x', 'y', 'z', 'vx', 'vy', 'vz'])
-    prns, test_pos, test_vel = measures._extract_pos_vel_arr(test_XYZV)
+    test_posvel = pd.DataFrame(
+        data = comb_array,
+        index = sv_names,
+        columns = ['times', 'x', 'y', 'z', 'vx', 'vy', 'vz']
+    )
+    prns, test_pos, test_vel = measures._extract_pos_vel_arr(test_posvel)
+
     np.testing.assert_array_equal(test_pos, pos_array)
     np.testing.assert_array_equal(test_vel, vel_array)
     np.testing.assert_array_equal(prns, np.array([1, 2, 3]))
 
+def test_find_elaz(expected_elaz, set_sat_pos, set_rx_pos):
+    calc_elaz = measures.find_elaz(set_rx_pos, set_sat_pos)
+    #print(calc_elaz)
+    np.testing.assert_array_almost_equal(expected_elaz, calc_elaz)
 
-
-def test_find_elaz():
-    gpsconsts = GPSConsts()
-    Rx = np.reshape(np.array([gpsconsts.A, 0, 0]), [1, 3])
-    test_XYZ = np.zeros([6, 3])
-    test_XYZ[0,0] = 1.25*gpsconsts.A
-    test_XYZ[1,0] = 0.75*gpsconsts.A
-    test_XYZ[2,0] = gpsconsts.A; test_XYZ[2,1]= gpsconsts.A
-    test_XYZ[3,0] = gpsconsts.A; test_XYZ[3,1]= -gpsconsts.A
-    test_XYZ[4,0] = gpsconsts.A; test_XYZ[4,2]= gpsconsts.A
-    test_XYZ[5,0] = gpsconsts.A; test_XYZ[5,2]= -gpsconsts.A
-    calc_elaz = measures.find_elaz(Rx, test_XYZ)
-    expect_elaz = np.zeros([6, 2])
-    expect_elaz[:, 0] = np.array([90, -90, 0, 0, 0, 0])
-    expect_elaz[:, 1] = np.array([0., 0., 90, -90, 0, 180])
-    np.testing.assert_array_almost_equal(expect_elaz, calc_elaz)
-
-
-def test_measures_value_range():
-    gpsconsts = GPSConsts()
-    measurements, satXYZV = static_test_measures()
+def test_measures_value_range(get_meas):
+    measurements, sat_posvel = get_meas
 
     assert np.logical_and(measurements['prange'].values > 20000e3,
         measurements['prange'].values < 3e7).all(), ("Invalid range of "
@@ -114,29 +143,56 @@ def test_measures_value_range():
         measurements['doppler'].values < 5000).all(), ("Magnitude of doppler "
         "values is greater than 5 KHz")
 
-    assert np.all(np.abs(satXYZV['x']).values < gpsconsts.A + 2e7), ("Invalid "
-        "range of ECEF x for satellite position")
+    assert np.all(np.abs(sat_posvel['x']).values < gpsconsts.A + 2e7),...
+    ("Invalid range of ECEF x for satellite position")
 
-    assert np.all(np.abs(satXYZV['y']).values < gpsconsts.A + 2e7), ("Invalid "
-        "range of ECEF y for satellite position")
+    assert np.all(np.abs(sat_posvel['y']).values < gpsconsts.A + 2e7),...
+    ("Invalid range of ECEF y for satellite position")
 
-    assert np.all(np.abs(satXYZV['z']).values < gpsconsts.A + 2e7), ("Invalid "
-        "range of ECEF z for satellite position")
+    assert np.all(np.abs(sat_posvel['z']).values < gpsconsts.A + 2e7),...
+    ("Invalid range of ECEF z for satellite position")
 
-def test_satV():
-    deltaT = 0.1
-    _, satXYZV_prev = static_test_measures()
-    _, satXYZV_curr = static_test_measures(deltaT=deltaT)
+def test_sat_velocity(get_meas, get_meas_dt):
+    _, sat_posvel_prev = get_meas
+    _, sat_posvel_new  = get_meas_dt
 
-    np.testing.assert_array_almost_equal((satXYZV_curr['y'] - satXYZV_prev['y'])
-    / deltaT, satXYZV_prev['vy'], decimal=1)
+    np.testing.assert_array_almost_equal(
+        (sat_posvel_new['x'] - sat_posvel_prev['x']) / dt,
+        sat_posvel_prev['vx'], decimal=1)
 
-    np.testing.assert_array_almost_equal((satXYZV_curr['x'] - satXYZV_prev['x'])
-    / deltaT, satXYZV_prev['vx'], decimal=1)
+    np.testing.assert_array_almost_equal(
+        (sat_posvel_new['y'] - sat_posvel_prev['y']) / dt,
+        sat_posvel_prev['vy'], decimal=1)
 
-    np.testing.assert_array_almost_equal((satXYZV_curr['z'] - satXYZV_prev['z'])
-    / deltaT, satXYZV_prev['vz'], decimal=1)
+    np.testing.assert_array_almost_equal(
+        (sat_posvel_new['z'] - sat_posvel_prev['z']) / dt,
+        sat_posvel_prev['vz'], decimal=1)
 
-def measure_sizes():
-    measurements, satXYZV = static_test_measures()
-    assert np.logical_and(len(measurements['prange'].index)==len(satXYZV.index))
+def test_measure_sizes(get_meas):
+    measurements, sat_posvel = get_meas
+    assert (len(measurements['prange'].index)==len(sat_posvel.index))
+
+def test_pseudorange_corrections(get_meas, get_meas_dt):
+    meas_prev, _ = get_meas
+    meas_new , _  = get_meas_dt
+
+    gpsweek = 2105
+    _, gpstime = datetime_to_tow(timestamp())
+
+    rx_ecef = np.reshape(Rx_ECEF(), [-1, 3])
+
+    ephem = extract_ephem()
+
+    sat_names = (meas_prev.index).tolist()
+    print(ephem.loc[sat_names,:])
+
+    meas_prev_corr = measures.correct_pseudorange(
+        gpstime   , gpsweek, ephem.loc[sat_names,:], meas_prev['prange'], rx_ecef)
+
+    meas_new_corr  = measures.correct_pseudorange(
+        gpstime+dt, gpsweek, ephem.loc[sat_names,:], meas_new['prange'] , rx_ecef)
+
+    diff_prev = meas_prev_corr - meas_prev['prange']
+    diff_new  = meas_new_corr  - meas_new['prange']
+
+    np.testing.assert_array_almost_equal(diff_prev, diff_new, decimal=2)
