172e8de1c0b6c930edc1ef2570b8b4bd9e904cea
kanhereashwin <kanhereashwin@users.noreply.github.com>
2022-01-24 22:22:48 -0800

    Made Shivam's tests consistent with repo

--- gnss_lib_py/core/filters.py
+++ gnss_lib_py/core/filters.py
@@ -31,10 +31,10 @@ class BaseFilter():
 class BaseKalmanFilter(BaseFilter):
 
     def __init__(self, init_dict, params_dict):
-        super.__init__(init_dict['x_dim'], init_dict['x0'], init_dict['P0'])
+        super().__init__(init_dict['x_dim'], init_dict['x0'], init_dict['P0'])
         assert check_square_mat(init_dict['Q'], self.x_dim)
         self.Q = init_dict['Q']
-        self.R = init_dict['R_scale']
+        self.R = init_dict['R']
         self.params_dict = params_dict
 
     def predict(self, u, predict_dict=None):
@@ -72,14 +72,14 @@ class BaseKalmanFilter(BaseFilter):
 class BaseExtendedKalmanFilter(BaseFilter):
 
     def __init__(self, init_dict, params_dict):
-        super.__init__(init_dict['x_dim'], init_dict['x0'], init_dict['P0'])
+        super().__init__(init_dict['x_dim'], init_dict['x0'], init_dict['P0'])
         assert check_square_mat(init_dict['Q'], self.x_dim)
         self.Q = init_dict['Q']
-        self.R = init_dict['R_scale']
+        self.R = init_dict['R']
         self.params_dict = params_dict
 
     def predict(self, u, predict_dict=None):
-        self.x = self.dyn_func(u, predict_dict) # Can pass parameters via predict_dict
+        self.x = self.dyn_model(u, predict_dict) # Can pass parameters via predict_dict
         A = self.linearize_dynamics(predict_dict)
         self.P = A @ self.P @ A.T + self.Q
         assert check_col_vect(self.x, self.x_dim), "Incorrect state shape after prediction"
@@ -90,7 +90,7 @@ class BaseExtendedKalmanFilter(BaseFilter):
         H = self.linearize_measurements(update_dict) # Can pass arguments via update_dict      
         S = H @ self.P @ H.T + self.R
         K = self.P @ H.T @ np.linalg.inv(S)
-        z_expect = self.expect_measure(update_dict) # Can pass arguments via update_dict
+        z_expect = self.measure_model(update_dict) # Can pass arguments via update_dict
         #Updating state
         self.x = self.x + K @ (z - z_expect)
         # Update covariance
@@ -109,7 +109,7 @@ class BaseExtendedKalmanFilter(BaseFilter):
         pass
 
     @abstractmethod
-    def meas_model(self, update_dict=None):
+    def measure_model(self, update_dict=None):
         pass
 
     @abstractmethod
---
+++ tests/core/test_filters.py
@@ -0,0 +1,144 @@
+"""Tests for a general EKF
+"""
+
+__authors__ = "Ashwin Kanhere, Shivam Soni"
+__date__ = "24 Januray 2020"
+
+import numpy as np
+import pandas as pd
+import math
+import pytest
+from numpy.random import default_rng
+
+from gnss_lib_py.core.filters import BaseExtendedKalmanFilter
+
+
+class MSD_EKF(BaseExtendedKalmanFilter):
+    #TODO: Define the state space, measurment space etc. here
+    def __init__(self, init_dict, params_dict):
+        self.rng = default_rng()
+        self.k = params_dict['k']
+        self.m = params_dict['m']
+        self.b = params_dict['b']
+        super().__init__(init_dict, params_dict)
+    
+    def linearize_dynamics(self, predict_dict=None):
+        A = np.array([[0, 1], [-self.k / self.m, -self.b / self.m]])
+        return A
+
+    def linearize_measurements(self, update_dict=None):
+        H = np.array([[1, 0]])
+        return H
+
+    def measure_model(self, update_dict=None):
+        meas = self.x[0]
+        return meas
+
+    def dyn_model(self, u, predict_dict=None):
+        A = self.linearize_dynamics()
+        B = np.zeros([2,1])
+        prop_noise = self.rng.multivariate_normal(np.zeros(self.x_dim), self.Q, size=1)
+        new_state = A @ self.x + B @ u + prop_noise.T
+        return new_state
+
+
+
+@pytest.fixture(name="times")
+def msd_times():
+    t_end = 20  # Seconds
+    time_step = 0.01
+    t_vals = np.linspace(0, t_end, int(1 / time_step) + 1)
+    return t_vals
+
+
+@pytest.fixture(name="x_exact")
+def msd_exact_sol(times):
+    zeta = 0.5
+    omega_n = 2
+    omega_d = omega_n * np.sqrt(1 - zeta ** 2)
+    y0 = 1
+    yd0 = 1
+    C1 = y0
+    C2 = (yd0 + zeta * omega_n * y0) / omega_d
+    C = np.sqrt(C1 ** 2 + C2 ** 2)
+    phi = np.arctan2(C1, C2)
+    exp_term = np.exp(-zeta * omega_n * times)
+    sin_term = np.sin(omega_d * times + phi)
+    exact_sol = exp_term * C * sin_term
+    return exact_sol
+
+
+@pytest.fixture(name="init_dict")
+def msd_ekf_params():
+    ekf_init_dict = {'x_dim': 2,
+                     'x0': np.array([[1], [1]]),
+                     'P0': np.eye(2)}
+    return ekf_init_dict
+
+
+@pytest.fixture(name="msd_params")
+def msd_params():
+    params = {'m': 1,
+            'b' : 2,
+            'k' : 4}
+
+
+def msd_filter_sol(times, x_exact, init_dict, q, r, filter_type):
+    if filter_type=='ekf':
+        init_dict['Q'] = q*np.eye(init_dict['x_dim'])
+        init_dict['R'] = r*np.eye(1)
+        params_dict = {'m': 1,
+                    'b' : 2,
+                    'k' : 4}
+        msd_filter = MSD_EKF(init_dict, params_dict)
+    else:
+        raise NotImplementedError
+    t_len = np.size(times)
+    u = np.array([[0]])
+    run_rng = default_rng()
+
+    x_filter = np.empty([0, 1])
+    P_pre = np.empty([0, 2, 2])
+    P_post = np.empty([0, 2, 2])
+    
+    for t_idx in range(t_len):
+        msd_filter.predict(u)
+        P_pre_temp = np.reshape(msd_filter.P, [1, msd_filter.x_dim, msd_filter.x_dim])
+        P_pre = np.concatenate([P_pre, P_pre_temp], axis=0)
+        z = np.reshape(x_exact[t_idx] + run_rng.normal(0, r, size=1), [-1, 1])
+        msd_filter.update(z)
+        # Save positions and covariance matrices
+        x_filter = np.append(x_filter, msd_filter.x[0])
+        P_post_temp = np.reshape(msd_filter.P, [1, msd_filter.x_dim, msd_filter.x_dim])
+        P_post = np.concatenate([P_post, P_post_temp], axis=0)
+    return x_filter, P_pre, P_post
+
+
+@pytest.mark.parametrize("filter_type", ['ekf'])
+def test_exact_sol(times, x_exact, init_dict, filter_type):
+    #TODO: Check why increasing the q value below from 0.001 to 0.01 makes the test pass
+    x_filter, _, _ = msd_filter_sol(times, x_exact, init_dict, 0.01, 0.0001, filter_type)
+    #TODO: Why are different indices being compared here? Add findings as a note
+    print('x_exact shape', x_exact.shape)
+    print('x_filter shape', x_filter.shape)
+    np.testing.assert_array_almost_equal(x_exact, x_filter, decimal=2)
+
+
+@pytest.mark.parametrize("filter_type", ['ekf'])
+@pytest.mark.parametrize('q, r',
+                        [(0.01, 0.00001),
+                        (0.00001, 0.01),
+                        (0.01, 0.01)])
+def test_filter_cov_tests(times, x_exact, init_dict, q, r, filter_type):
+    x_filter, P_pre, P_post = msd_filter_sol(times, x_exact, init_dict, q, r, filter_type)
+    # Test that all P matrices are positive semidefinite
+    for idx in range(np.shape(P_pre)[0]):
+        # Cholesky factorization will fail if matrix is not PSD
+        np.linalg.cholesky(P_pre[idx, :, :])
+        np.linalg.cholesky(P_post[idx, :, :])
+    # Covariance should decrease after EKF update
+    np.testing.assert_array_less(P_post[:, 0, 0], P_pre[:, 0, 0])
+    np.testing.assert_array_less(P_post[:, 1, 1], P_pre[:, 1, 1])
+    # Covariance should increase after EKF prediction
+    np.testing.assert_array_less(P_post[:-1, 0, 0], P_pre[1:, 0, 0])
+    np.testing.assert_array_less(P_post[:-1, 1, 1], P_pre[1:, 1, 1])
