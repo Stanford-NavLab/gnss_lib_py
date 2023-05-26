65f79fd5e14432abe7a817318fc564bd22ee7b36
Dalton Vega <daltonv297@gmail.com>
2023-05-26 13:46:24 -0700

    Made NMEA parsing NavData compatible

--- gnss_lib_py/parsers/nmea.py
+++ gnss_lib_py/parsers/nmea.py
@@ -7,15 +7,20 @@ __date__ = "16 Jul 2021"
 
 import pynmea2
 import numpy as np
+import pandas as pd
 import calendar
+import datetime
 
+from gnss_lib_py.parsers.navdata import NavData
 from gnss_lib_py.utils import coordinates as coord
+from gnss_lib_py.utils.time_conversions import datetime_to_gps_millis
+from pynmea2.nmea_utils import timestamp, datestamp
 
-class NMEA():
+class NMEA(NavData):
     """Class used to parse through NMEA files
 
     """
-    def __init__(self, filename):
+    def __init__(self, filename, msg_types=['GGA', 'RMC'], float_coords=True):
         """Initialize NMEA class.
 
         Parameters
@@ -24,18 +29,56 @@ class NMEA():
             filepath to .NMEA file to read
 
         """
-        self.gga_msgs = []
-        self.rmc_msgs = []
+        pd_df = pd.DataFrame()
+        field_dict = {}
+        prev_timestamp = None
         with open(filename, "r") as f:
             for line in f:
                 try:
                     msg = pynmea2.parse(line, check = False)
-                    if type(msg) == pynmea2.GGA:
-                        self.gga_msgs.append(msg)
-                    elif type(msg) == pynmea2.RMC:
-                        self.rmc_msgs.append(msg)
+                    if 'timestamp' in list(msg.name_to_idx.keys()):
+                        # find first timestamp
+                        if prev_timestamp == None:
+                            prev_timestamp = msg.timestamp
+
+                        elif msg.timestamp != prev_timestamp:
+                            # TODO: put in try/except block if there is no date
+                            # convert timestamp and datestamp into gps_millis
+                            time = field_dict.pop('timestamp')
+                            date = field_dict.pop('datestamp')
+
+                            dt = datetime.datetime.combine(datestamp(date), timestamp(time))
+                            field_dict['gps_millis'] = datetime_to_gps_millis(dt)
+
+                            new_row = pd.DataFrame([field_dict])
+                            pd_df = pd.concat([pd_df, new_row])
+                            field_dict = {}
+                            prev_timestamp = msg.timestamp
+                    if msg.sentence_type in msg_types:
+                        if float_coords:
+                            ignore = ['lat', 'lat_dir', 'lon', 'lon_dir']
+                            try:
+                                field_dict['lat'] = msg.latitude
+                                field_dict['lon'] = msg.longitude
+                            except NameError:
+                                pass
+                        else:
+                            ignore = []
+                        for field in msg.name_to_idx:
+                            if field not in ignore:
+                                field_dict[field] = msg.data[msg.name_to_idx[field]]
                 except pynmea2.ChecksumError as e:
                     pass
+
+            time = field_dict.pop('timestamp')
+            date = field_dict.pop('datestamp')
+            dt = datetime.datetime.combine(datestamp(date), timestamp(time))
+            field_dict['gps_millis'] = datetime_to_gps_millis(dt)
+            new_row = pd.DataFrame([field_dict])
+            pd_df = pd.concat([pd_df, new_row])
+            
+        super().__init__(pandas_df=pd_df)
+    
 
     def ecef_gt_w_time(self, date):
         """Get ECEF ground truth as well as measurement timesself.
