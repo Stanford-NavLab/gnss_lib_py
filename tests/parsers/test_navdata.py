fed47558b6190b19e35b29fdd038f67d883e93dc
kanhereashwin <kanhereashwin@users.noreply.github.com>
2023-06-14 17:08:04 -0700

    Update test_navdata.py

--- tests/parsers/test_navdata.py
+++ tests/parsers/test_navdata.py
@@ -2403,3 +2403,13 @@ def test_sort(data, df_simple):
     data_sorted_int_des.sort('integers', ascending=False, inplace=True)
     data_sorted_int_des = data_sorted_int_des.pandas_df()
     pd.testing.assert_frame_equal(df_sorted_int_des, data_sorted_int_des)
+
+    # Test sorting for only one column
+    navdata_single_col = NavData()
+    navdata_single_col['name'] = 'NAVLab'
+    navdata_single_col['number'] = 1
+    navdata_single_col['weight'] = 100
+    sorted_single_col = navdata_single_col.sort()
+    pd.testing.assert_frame_equal(sorted_single_col.pandas_df(),
+                                navdata_single_col.pandas_df())
+
