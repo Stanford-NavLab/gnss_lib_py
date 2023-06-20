95e68f746b20402bcc3b99ac006c84e2a3167070
betaBison <knowles.derek@gmail.com>
2023-06-19 21:28:36 -0700

    fixes #106, tuple getitem

--- tests/parsers/test_navdata.py
+++ tests/parsers/test_navdata.py
@@ -903,11 +903,18 @@ def fixture_flt_int_slc(df_rows):
                         (('integers', slice(None, None)),
                           lazy_fixture('integers')),
                         (['integers', 'floats'], lazy_fixture('int_flt')),
+                        (('integers', 'floats'), lazy_fixture('int_flt')),
+                        ((['integers', 'floats'], 0), np.array([10., 0.5])),
+                        ((('integers', 'floats'), 0), np.array([10., 0.5])),
                         (('integers', 0), 10.),
                         (('strings', 0), np.asarray([['gps']], dtype=object)),
                         (['names', 'strings'], lazy_fixture('nm_str')),
+                        (('names', 'strings'), lazy_fixture('nm_str')),
                         (['strings', 'names'], lazy_fixture('str_nm')),
+                        (('strings', 'names'), lazy_fixture('str_nm')),
                         ((['integers', 'floats'], slice(3, None)),
+                           lazy_fixture('flt_int_slc')),
+                        ((('integers', 'floats'), slice(3, None)),
                            lazy_fixture('flt_int_slc')),
                         (1, lazy_fixture('integers'))
                         ])
